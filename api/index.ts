47af4953c3b8752d3b833b584b7e71529089b5f7
Filip <harajfilip.co@gmail.com>
2026-06-01 21:36:33 +0200

    add vercel config

---
+++ api/index.ts
@@ -0,0 +1,24 @@
+import express, { type Request, Response, NextFunction } from "express";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+
+app.use(express.json({
+  verify: (req: any, _res, buf) => {
+    req.rawBody = buf;
+  },
+}));
+app.use(express.urlencoded({ extended: false }));
+
+const ready = registerRoutes(app);
+
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  const status = err.status || err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({ message });
+});
+
+export default async function handler(req: Request, res: Response) {
+  await ready;
+  app(req, res);
+}
